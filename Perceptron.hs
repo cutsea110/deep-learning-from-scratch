44ac98fb7d422cadfd5e4d12637d294d47a6f1ad
cutsea110 <cutsea110@gmail.com>
2019-02-20 22:29:03 +0900

    init

---
+++ Perceptron.hs
@@ -0,0 +1,14 @@
+module Perceptron where
+
+
+pAND x1 x2 | tmp <= theta = 0
+           | otherwise    = 1
+  where
+    (w1, w2, theta) = (0.5, 0.5, 0.7)
+    tmp = x1 * w1 + x2 * w2
+
+pNAND x1 x2 | tmp <= theta = 0
+            | otherwise    = 1
+  where
+    (w1, w2, theta) = (-0.5, -0.5, -0.7)
+    tmp = x1 * w1 + x2 * w2
