7cd8ff3dd4636a70829f589dec082fabea48f730
cutsea110 <cutsea110@gmail.com>
2019-02-20 23:22:08 +0900

    study repa

--- Perceptron.hs
+++ Perceptron.hs
@@ -1,6 +1,9 @@
 module Perceptron where
 
+import Control.Monad
+import Control.Monad.ST
 import qualified Data.Array.Repa as R
+import Data.Array.Repa.Algorithms.Matrix
 import Data.Array.Repa.Algorithms.Randomish
 
 pAND x1 x2 | tmp <= theta = 0
@@ -28,6 +31,14 @@ main :: IO ()
 main = do
   putStrLn "This is sample of Repa."
   print rands
+  print $ mmultS y v1
+  print $ runST $ mmultP y v1
+
+y :: R.Array R.U R.DIM2 Double
+y = R.fromListUnboxed (R.Z R.:. 2 R.:. 2) [1..4]
+
+v1 :: R.Array R.U R.DIM2 Double
+v1 = R.fromListUnboxed (R.Z R.:. 2 R.:. 1) [5,6]
 
 rands :: R.Array R.U R.DIM1 Double
 rands = randomishDoubleArray (R.Z R.:. 10) 0.4 0.6 1
