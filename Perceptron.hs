4b1549509efcb73f96235ffa9995818d5775c378
cutsea110 <cutsea110@gmail.com>
2019-02-20 22:32:13 +0900

    OR

--- Perceptron.hs
+++ Perceptron.hs
@@ -12,3 +12,10 @@ pNAND x1 x2 | tmp <= theta = 0
   where
     (w1, w2, theta) = (-0.5, -0.5, -0.7)
     tmp = x1 * w1 + x2 * w2
+
+pOR x1 x2 | tmp <= theta = 0
+          | otherwise    = 1
+  where
+    (w1, w2, theta) = (0.5, 0.5, 0.2)
+    tmp = x1 * w1 + x2 * w2
+
