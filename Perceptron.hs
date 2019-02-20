4ade12925a230114391e824071e0aecbe4eee187
cutsea110 <cutsea110@gmail.com>
2019-02-20 23:56:18 +0900

    [Refactor] genPerceptron

--- Perceptron.hs
+++ Perceptron.hs
@@ -6,30 +6,23 @@ import qualified Data.Array.Repa as R
 import Data.Array.Repa.Algorithms.Matrix
 import Data.Array.Repa.Algorithms.Randomish
 
-pAND x1 x2 | tmp <= 0  = 0
-           | otherwise = 1
+genPerceptron (w1, w2, bias) = perceptron
   where
-    x, w :: R.Array R.U R.DIM2 Double
-    x = R.fromListUnboxed (R.Z R.:. 2 R.:. 1) [x1, x2]
-    w = R.fromListUnboxed (R.Z R.:. 2 R.:. 1) [0.5, 0.5]
-    b = -0.7
---    tmp = b + R.sumAllS (mmultS x w)
-    tmp = runST $ do
-      t <- mmultP x w
-      s <- R.sumAllP t
-      return $ b + s
+    perceptron x1 x2 | tmp <= 0  = 0
+                     | otherwise = 1
+      where
+        x, w :: R.Array R.U R.DIM2 Double
+        x = R.fromListUnboxed (R.Z R.:. 2 R.:. 1) [x1, x2]
+        w = R.fromListUnboxed (R.Z R.:. 2 R.:. 1) [w1, w2]
+        -- tmp = b + R.sumAllS (mmultS x w)
+        tmp = runST $ do
+          t <- mmultP x w
+          s <- R.sumAllP t
+          return $ bias + s
 
-pNAND x1 x2 | tmp <= theta = 0
-            | otherwise    = 1
-  where
-    (w1, w2, theta) = (-0.5, -0.5, -0.7)
-    tmp = x1 * w1 + x2 * w2
-
-pOR x1 x2 | tmp <= theta = 0
-          | otherwise    = 1
-  where
-    (w1, w2, theta) = (0.5, 0.5, 0.2)
-    tmp = x1 * w1 + x2 * w2
+pAND  = genPerceptron ( 0.5,  0.5, -0.7)
+pNAND = genPerceptron (-0.5, -0.5,  0.7)
+pOR   = genPerceptron ( 0.5,  0.5, -0.2)
 
 x :: R.Array R.U R.DIM2 Double
 x = R.fromListUnboxed (R.Z R.:. 3 R.:. 2) [0..5]
