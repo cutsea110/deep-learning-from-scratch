a949cea001eb5f071b24aedf48f88830991f8205
cutsea110 <cutsea110@gmail.com>
2019-02-25 09:42:20 +0900

    restore Test module

--- Test.hs
+++ Test.hs
@@ -1,11 +1,11 @@
 module Test where
 
 import Graphics.Gnuplot.Simple
-import Neuron ( perceptronFunction
-              , sigmoidFunction
-              , stepFunction
-              , reluFunction
-              )
+import Activation ( perceptron
+                  , sigmoid
+                  , step
+                  , relu
+                  )
 plot :: (Double -> Double) -> (Double, Double) -> IO ()
 plot f (l, r) = plotPath [] $ zip <$> id <*> fmap f $ linearScale points (l, r)
   where points = round ((r - l)/4.0e-3)
@@ -14,13 +14,13 @@ plotSin :: IO ()
 plotSin = plot sin (0.0, 2*pi)
 
 plotPerceptron :: IO ()
-plotPerceptron = plot perceptronFunction (-2.0, 2.0)
+plotPerceptron = plot perceptron (-2.0, 2.0)
 
 plotSigmoid :: IO ()
-plotSigmoid = plot sigmoidFunction (-10.0, 10.0)
+plotSigmoid = plot sigmoid (-10.0, 10.0)
 
 plotStep :: IO ()
-plotStep = plot stepFunction (-2.0, 2.0)
+plotStep = plot step (-2.0, 2.0)
 
 plotReLU :: IO ()
-plotReLU = plot reluFunction (-2.0, 5.0)
+plotReLU = plot relu (-2.0, 5.0)
