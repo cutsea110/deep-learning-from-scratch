0883d3332167e2631d503150b16cf04ecbecccb3
cutsea110 <cutsea110@gmail.com>
2019-02-26 16:08:56 +0900

    [FIX] compile error occurred.

--- Test.hs
+++ Test.hs
@@ -1,5 +1,7 @@
  module Test where
 
+import qualified Data.Array.Repa as R
+ 
 import Activation ( perceptron
                   , sigmoid
                   , step
@@ -8,24 +10,25 @@ import Activation ( perceptron
 import Util
 
 plotSin :: IO ()
-plotSin = plot sin (0.0, 2*pi)
+plotSin = plot sin (0.0 :: Double, 2*pi)
 
 plotPerceptron :: IO ()
-plotPerceptron = plot perceptron (-2.0, 2.0)
+plotPerceptron = plot perceptron (-2.0 :: Double, 2.0)
 
 plotSigmoid :: IO ()
-plotSigmoid = plot sigmoid (-10.0, 10.0)
+plotSigmoid = plot sigmoid (-10.0 :: Double, 10.0)
 
 plotStep :: IO ()
-plotStep = plot step (-2.0, 2.0)
+plotStep = plot step (-2.0 :: Double, 2.0)
 
 plotReLU :: IO ()
-plotReLU = plot relu (-2.0, 5.0)
+plotReLU = plot relu (-2.0 :: Double, 5.0)
 
+fun1 :: Double -> Double
 fun1 x = 0.01 * (x^2) + 0.1 * x
 
 plotFun1 :: IO ()
-plotFun1 = plot fun1 (0.0,20.0)
+plotFun1 = plot fun1 (0.0, 20.0)
 
 -- gradient at (5, 0.75(= fun1 5))
 plotWithGrad1 :: IO ()
@@ -37,3 +40,8 @@ plotWithGrad2 = plots [fun1, genGrad fun1 10] (0.0, 20.0)
 
 plotWithGrad3 :: IO ()
 plotWithGrad3 = plots [fun1, genGrad fun1 1, genGrad fun1 5, genGrad fun1 10] (0.0, 20.0)
+
+numGrad1 = R.computeUnboxedS $ numericalGradient f (R.fromListUnboxed (R.Z R.:.2) [3.0, 4.0] :: R.Array R.U R.DIM1 Double)
+  where
+    f x = R.sumAllS $ R.map (^2) x
+
