2a198a0501b134a885d7cb37742996636f3aad4a
cutsea110 <cutsea110@gmail.com>
2019-03-04 07:52:59 +0900

    row extend?

--- RepaTest.hs
+++ RepaTest.hs
@@ -3,10 +3,11 @@ module RepaTest where
 import Data.Array.Repa
 
 xs :: Array U DIM2 Int
-xs = fromListUnboxed (ix2 2 2) [1..4]
+xs = fromListUnboxed (ix2 2 3) [1..6]
 
 col0 = computeUnboxedS $ slice xs (Any :. (0 :: Int))
 col1 = computeUnboxedS $ slice xs (Any :. (1 :: Int))
+col2 = computeUnboxedS $ slice xs (Any :. (2 :: Int))
 
 row0 = computeUnboxedS $ slice xs (Z :. (0::Int) :. All)
 row1 = computeUnboxedS $ slice xs (Z :. (1::Int) :. All)
@@ -14,3 +15,5 @@ row1 = computeUnboxedS $ slice xs (Z :. (1::Int) :. All)
 colextend2 = computeUnboxedS $ extend (Any :.(2::Int)) xs
 colextend3 = computeUnboxedS $ extend (Any :.(3::Int)) xs
 
+rowextend2 = computeUnboxedS $ extend (Any :. (2::Int) :. All) xs
+rowextend3 = computeUnboxedS $ extend (Any :. (3::Int) :. All) xs
