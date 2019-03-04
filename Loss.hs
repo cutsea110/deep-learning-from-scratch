c950d254829b80e9e1be79ad4d44119f8aa98098
cutsea110 <cutsea110@gmail.com>
2019-03-04 11:16:10 +0900

    Add type signature

--- Loss.hs
+++ Loss.hs
@@ -10,24 +10,27 @@ import qualified Data.Vector.Unboxed
 
 -- | mean squared error
 mseS :: (Fractional a, R.Shape sh, R.Source r1 a, R.Source r2 a) =>
-        R.Array r1 sh a -> R.Array r2 sh a -> a
+  R.Array r1 sh a -> R.Array r2 sh a -> a
 mseS y t = 0.5 * (R.sumAllS $ R.map (^2) $ y R.-^ t)
 
 -- | mean squared error
-mseP :: (Monad m, R.Shape sh, Data.Vector.Unboxed.Unbox b,
-         R.Source r1 b, R.Source r2 b, Fractional b) =>
-        R.Array r1 sh b -> R.Array r2 sh b -> m b
+mseP :: (Monad m, R.Shape sh, Data.Vector.Unboxed.Unbox b, R.Source r1 b, R.Source r2 b, Fractional b) =>
+  R.Array r1 sh b -> R.Array r2 sh b -> m b
 mseP y t = do
   x <- R.sumAllP $ R.map (^2) $ y R.-^ t
   return (0.5 * x)
 
 -- | cross entropy error
+ceeS :: (R.Shape sh, R.Source r a, R.Source r1 a, Floating a) =>
+  R.Array r sh a -> R.Array r1 sh a -> a
 ceeS y t = negate (R.sumAllS $ t R.*^ R.map (log . (+delta)) y) / (fromIntegral batchSize)
   where
     delta = 1e-7
     batchSize = last $ R.listOfShape $ R.extent y
 
 -- | cross entropy error
+ceeP :: (R.Shape sh, Monad m, Data.Vector.Unboxed.Unbox b, R.Source r b, R.Source r1 b, Floating b) =>
+  R.Array r sh b -> R.Array r1 sh b -> m b
 ceeP y t = do
   x <- R.sumAllP $ t R.*^ R.map (log . (+delta)) y
   return (negate x / fromIntegral batchSize)
