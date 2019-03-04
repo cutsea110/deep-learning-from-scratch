68903855f11b6153bc13b28bb8c55060dd8edea4
cutsea110 <cutsea110@gmail.com>
2019-03-04 13:06:12 +0900

    testing...

--- RepaTest.hs
+++ RepaTest.hs
@@ -51,3 +51,34 @@ rowextend2 = computeUnboxedS $ extend (Z :. (2::Int) :. All :. All) xs
 -- ,[[1,2,3]
 --  ,[4,5,6]]]
 rowextend3 = computeUnboxedS $ extend (Z :. (3::Int) :. All :. All) xs
+
+x1 :: Array U DIM1 Double
+x1 = fromListUnboxed (ix1 3) [1..3]
+
+x2 :: Array U DIM2 Double
+x2 = fromListUnboxed (ix2 2 3) [1..6]
+
+b1 :: Array U Z Double
+b1 = fromListUnboxed Z [0.5]
+
+b2 :: Array U DIM2 Double
+b2 = fromListUnboxed (ix2 2 1) [10,100]
+
+b3 :: Array U DIM2 Double
+b3 = fromListUnboxed (ix2 1 3) [10,20,30]
+
+-- x1 :: Z :. 3
+-- b1 :: Z
+x1b1 = computeUnboxedS $ x1 +^ extend (Any :. (3::Int)) b1
+
+-- x2 :: Z :. 2 :. 3
+-- b2 :: Z :. 2 :. 1
+x2b2 = computeUnboxedS $ x2 +^ reshape (ix2 2 3) (extend (Any :. (3::Int)) b2)
+
+-- x2 :: Z :. 2 :. 3
+-- b3 :: Z :. 1 :. 3
+x2b3 = computeUnboxedS $ x2 +^ reshape (ix2 2 3) (extend (Z :. (2::Int) :. All :. All) b3)
+
+-- x2 :: Z :. 2 :. 3
+-- b1 :: Z
+x2b1 = computeUnboxedS $ x2 +^ extend (Any :. (2::Int) :. (3::Int)) b1
